dc686c1c7489f6c86107c2aada7dc9f97c454a47
TheGreatJakester <jacob.moulton@gmail.com>
2023-12-03 21:58:19 -0700

    dump old

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,50 +1,139 @@
-import utils.readInputAsLines
+package day14
 
-val possibleHands = mapOf(
-    "A X" to 4,
-    "B X" to 1,
-    "C X" to 7,
+import utils.Point
+import utils.readInputAsText
+import utils.runSolver
+import utils.string.asLines
 
-    "A Y" to 8,
-    "B Y" to 5,
-    "C Y" to 2,
+private typealias SolutionType = Int
 
-    "A Z" to 3,
-    "B Z" to 9,
-    "C Z" to 6
+private const val defaultSolution = 0
+
+private const val dayNumber: String = "14"
+private val testSolution1: SolutionType? = 24
+private val testSolution2: SolutionType? = 93
+
+
+fun Point.pointsBellow() = listOf(
+    first to second + 1,
+    first - 1 to second + 1,
+    first + 1 to second + 1
 )
 
-val possibleHandsStrat2 = mapOf(
-    "A X" to 3,
-    "B X" to 1,
-    "C X" to 2,
+val generationPoint = 500 to 0
 
-    "A Y" to 4,
-    "B Y" to 5,
-    "C Y" to 6,
-
-    "A Z" to 6+2,
-    "B Z" to 6+3,
-    "C Z" to 6+1
-)
-fun main() {
-    fun part1(input: List<String>): Int {
-        val hands = input.mapNotNull { possibleHands[it] }
-        return hands.sum()
+private fun getCaveFromInput(input: String): MutableSet<Point> {
+    val linePairs = input.asLines().map { line ->
+        line.split(" -> ").map { pointText ->
+            val c = pointText.split(",")
+            c[0].toInt() to c[1].toInt()
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        val hands = input.mapNotNull { possibleHandsStrat2[it] }
-        return hands.sum()
+    val cavePoints = mutableSetOf<Point>()
+
+    infix fun Int.overTo(other: Int): IntRange {
+        if (this > other) {
+            return other..this
+        }
+        return this..other
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInputAsLines("Day02_test")
-    check(part1(testInput) == 15)
-    check(part2(testInput) == 12)
+    fun addLine(from: Point, to: Point) {
+        // for vertical lines
+        if (from.first == to.first) {
+            for (y in from.second overTo to.second) {
+                cavePoints.add(from.first to y)
+            }
+            return
+        }
 
-    val input = readInputAsLines("Day02")
-    println(part1(input))
-    println(part2(input))
+        val slope = (from.second - to.second).toFloat() / (from.first - to.first)
+
+        for (x in from.first overTo to.first) {
+            val y = (slope * (x - from.first)).toInt() + to.second
+            cavePoints.add(x to y)
+        }
+    }
+
+
+    linePairs.forEach { line ->
+        line.windowed(2) {
+            addLine(it[0], it[1])
+        }
+    }
+
+
+    return cavePoints
 }
 
+private fun part1(input: String): SolutionType {
+    val filledPoints = getCaveFromInput(input)
+
+    var sandCount = 0
+
+    fun isSandDeep(sand: Point) = sand.second > 1000
+
+    tailrec fun nextSandPoint(sand: Point = generationPoint): Point {
+        if (isSandDeep(sand)) return sand
+
+        sand.pointsBellow().firstOrNull {
+            !filledPoints.contains(it)
+        }?.let {
+            return nextSandPoint(it)
+        }
+
+        return sand
+    }
+
+    while (true) {
+        val sand = nextSandPoint()
+        if (isSandDeep(sand)) break
+
+        filledPoints.add(sand)
+        sandCount += 1
+    }
+
+    return sandCount
+}
+
+private fun part2(input: String): SolutionType {
+    val filledPoints = getCaveFromInput(input)
+    var sandCount = 1
+    val floorLevel = filledPoints.maxOf { it.second } + 2
+    fun isSandOnFloor(sand: Point) = sand.second + 1 == floorLevel
+
+    tailrec fun nextSandPoint(sand: Point = generationPoint): Point {
+        if (isSandOnFloor(sand)) return sand
+
+        sand.pointsBellow().firstOrNull {
+            !filledPoints.contains(it)
+        }?.let {
+            return nextSandPoint(it)
+        }
+
+        return sand
+    }
+
+    while (true) {
+        val sand = nextSandPoint()
+        if (sand == generationPoint) break
+
+        filledPoints.add(sand)
+        sandCount += 1
+    }
+
+    val sandOnFloor = filledPoints.count(::isSandOnFloor)
+    println("There are $sandOnFloor grains of sand on the floor of the cave")
+
+    return sandCount
+}
+
+
+fun main() {
+    runSolver("Test 1", readInputAsText("Day${dayNumber}_test"), testSolution1, ::part1)
+    runSolver("Part 1", readInputAsText("Day${dayNumber}"), null, ::part1)
+
+    runSolver("Test 2", readInputAsText("Day${dayNumber}_test"), testSolution2, ::part2)
+    runSolver("Part 2", readInputAsText("Day${dayNumber}"), null, ::part2)
+}
