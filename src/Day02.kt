eb8fbe7250a31ecc857ea3f40a11d38f97116f02
TheGreatJakester <jacob.moulton@gmail.com>
2023-12-03 21:58:39 -0700

    Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,139 +1,97 @@
-package day14
+package day2
 
-import utils.Point
-import utils.readInputAsText
-import utils.runSolver
+import kotlinx.coroutines.runBlocking
+import utils.*
 import utils.string.asLines
+import java.awt.Color
 
-private typealias SolutionType = Int
+private typealias SolutionType = Long
 
 private const val defaultSolution = 0
 
-private const val dayNumber: String = "14"
-private val testSolution1: SolutionType? = 24
-private val testSolution2: SolutionType? = 93
+private const val dayNumber: String = "02"
+private val testSolution1: SolutionType? = 8
+private val testSolution2: SolutionType? = 2286
 
 
-fun Point.pointsBellow() = listOf(
-    first to second + 1,
-    first - 1 to second + 1,
-    first + 1 to second + 1
-)
-
-val generationPoint = 500 to 0
-
-private fun getCaveFromInput(input: String): MutableSet<Point> {
-    val linePairs = input.asLines().map { line ->
-        line.split(" -> ").map { pointText ->
-            val c = pointText.split(",")
-            c[0].toInt() to c[1].toInt()
-        }
-    }
-
-    val cavePoints = mutableSetOf<Point>()
-
-    infix fun Int.overTo(other: Int): IntRange {
-        if (this > other) {
-            return other..this
-        }
-        return this..other
-    }
-
-    fun addLine(from: Point, to: Point) {
-        // for vertical lines
-        if (from.first == to.first) {
-            for (y in from.second overTo to.second) {
-                cavePoints.add(from.first to y)
-            }
-            return
-        }
-
-        val slope = (from.second - to.second).toFloat() / (from.first - to.first)
-
-        for (x in from.first overTo to.first) {
-            val y = (slope * (x - from.first)).toInt() + to.second
-            cavePoints.add(x to y)
-        }
-    }
-
-
-    linePairs.forEach { line ->
-        line.windowed(2) {
-            addLine(it[0], it[1])
-        }
-    }
-
-
-    return cavePoints
+fun colorToMax(color: String) = when (color) {
+    "red" -> 12
+    "green" -> 13
+    "blue" -> 14
+    else -> throw Exception("Missing color: $color")
 }
 
+fun isCountOverMax(color: String, count: Int): Boolean = colorToMax(color) < count
+
+
+fun MutableMap<String, Int>.applyNewMax(color: String, count: Int) {
+    val old = getOrDefault(color, 0)
+    if (count > old) {
+        put(color, count)
+    }
+}
+
+fun Map<*, Int>.productOfValues(): Long = this.values.fold(1L) { acc, i ->
+    acc * i
+}
+
+
 private fun part1(input: String): SolutionType {
-    val filledPoints = getCaveFromInput(input)
+    val sum = input.asLines().sumOf {
 
-    var sandCount = 0
+        val (gameId, sets) = it.split(":")
 
-    fun isSandDeep(sand: Point) = sand.second > 1000
+        sets.split(";").forEach { piles ->
 
-    tailrec fun nextSandPoint(sand: Point = generationPoint): Point {
-        if (isSandDeep(sand)) return sand
-
-        sand.pointsBellow().firstOrNull {
-            !filledPoints.contains(it)
-        }?.let {
-            return nextSandPoint(it)
+            piles.split(",").forEach { pile ->
+                val (count, color) = pile.trim().split(" ")
+                if (isCountOverMax(color, count.trim().toInt())) return@sumOf 0
+            }
         }
 
-        return sand
+        return@sumOf gameId.split(" ").last().toInt()
     }
 
-    while (true) {
-        val sand = nextSandPoint()
-        if (isSandDeep(sand)) break
 
-        filledPoints.add(sand)
-        sandCount += 1
-    }
-
-    return sandCount
+    return sum.toLong()
 }
 
 private fun part2(input: String): SolutionType {
-    val filledPoints = getCaveFromInput(input)
-    var sandCount = 1
-    val floorLevel = filledPoints.maxOf { it.second } + 2
-    fun isSandOnFloor(sand: Point) = sand.second + 1 == floorLevel
+    val sum = input.asLines().sumOf {
+        val maxs = mutableMapOf<String, Int>()
+        val (gameId, sets) = it.split(":")
 
-    tailrec fun nextSandPoint(sand: Point = generationPoint): Point {
-        if (isSandOnFloor(sand)) return sand
+        sets.split(";").forEach { set ->
 
-        sand.pointsBellow().firstOrNull {
-            !filledPoints.contains(it)
-        }?.let {
-            return nextSandPoint(it)
+            set.split(",").forEach { pile ->
+                val (count, color) = pile.trim().split(" ")
+                maxs.applyNewMax(color, count.toInt())
+            }
+
+
         }
 
-        return sand
+        return@sumOf maxs.productOfValues()
     }
 
-    while (true) {
-        val sand = nextSandPoint()
-        if (sand == generationPoint) break
 
-        filledPoints.add(sand)
-        sandCount += 1
-    }
-
-    val sandOnFloor = filledPoints.count(::isSandOnFloor)
-    println("There are $sandOnFloor grains of sand on the floor of the cave")
-
-    return sandCount
+    return sum
 }
 
 
 fun main() {
-    runSolver("Test 1", readInputAsText("Day${dayNumber}_test"), testSolution1, ::part1)
+    runBlocking {
+        checkOrGetInput(day = 2)
+    }
+    runSolver("Test 1", sampleInput, testSolution1, ::part1)
     runSolver("Part 1", readInputAsText("Day${dayNumber}"), null, ::part1)
 
-    runSolver("Test 2", readInputAsText("Day${dayNumber}_test"), testSolution2, ::part2)
+    runSolver("Test 2", sampleInput, testSolution2, ::part2)
     runSolver("Part 2", readInputAsText("Day${dayNumber}"), null, ::part2)
 }
+
+val sampleInput = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" +
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" +
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" +
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" +
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
